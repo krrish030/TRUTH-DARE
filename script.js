98459dea5ae6140027ab0a444f5205cd05b1e08b
krrish solanki <krrishsolanki030@gmail.com>
2026-02-17 14:36:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Truth or Dare</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <h1>Truth or Dare</h1>
+
+  <div class="box">
+    <button onclick="getTruth()">TRUTH</button>
+    <button onclick="getDare()">DARE</button>
+
+    <p id="result">Click Truth or Dare 😎</p>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
